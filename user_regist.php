082adbab73d90b1ae7a4957aac3b4a76e29506af
7Yong <95311188+7Yong@users.noreply.github.com>
2022-12-07 13:54:43 +0900

    Update user_regist.php

--- user_regist.php
+++ user_regist.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script src="http://code.jquery.com/jquery-latest.js"></script> 
+    <script src="https://code.jquery.com/jquery-latest.js"></script> 
     <style>
         #modal{
             display : none;
@@ -20,7 +20,7 @@
             
         }
         #modal>#content{
-            width:300px;
+            width:330px;
             margin:100px auto;
             padding:20px;
             position: relative;
@@ -46,36 +46,49 @@
     <title>頭がズキズキするのはどんなとき？</title>
 </head>
 <body>
-    <h1 style="text-align: center"><img width="388" src="./images/ptitle.png" alt="みんなの症状ことば"></h1><br>
-    <h2 style="text-align: center">頭がズキズキするのは<br>どんなとき？</h2><br>
-    
+<div style="line-height:30%">
+<br>
+<br>
+    <h1 style="text-align: center;font-size:16px">
+    <a href="https://anq.medicalkotoba.com/" height="5" width="10">
+      <img src="./images/newlogo.png" alt="みんなの症状ことば" />
+    <a>
+  </h1>
+  <br>
+    </div>
+    <h3 style="text-align: center;font-size:16px"><span style="color:#e52a94"><b>頭がズキズキする</span>のは<br>どんなとき</b>？</h3><br>
+
     <form action = "room_menu.php" method="get" name = "send_info" onsubmit="return checkCookies()">
             <div calss="row">
                 <div style = "margin:auto" class="col-sm-9">
                 <div style = "margin:auto" class="form-floating col-sm-3">
-                    <input type="text" style="WIDTH: 200pt" class="form-control" id = "room_name" placeholder="input_text" name="room_name" required>
-                    <button type="submit" style="WIDTH: 200pt; HEIGHT: 50pt" class="btn btn-danger" id = "submit_text">みんなと<br>つながる</button>
+                    <input type="text" style="WIDTH: 100%" class="form-control" id = "room_name" name="room_name" required required oninput="this.setCustomValidity('')" oninvalid="this.setCustomValidity('容を入力してください。');">
+                    <br>
+                    <button type="submit" class="btn text-white" style="background-color: #00bbff;WIDTH: 100%" id = "submit_text">みんなとつながる</button>
                 </div>
                 </div>
             </div>
     </form>
+    <br>
     <form action = "">
             <div calss="row">
                 <div style = "margin:auto" class="col-sm-9">
                 <div style = "margin:auto" class="form-floating col-sm-3">
-                    <button type="button" class="btn text-white" style="background-color: purple;WIDTH: 200pt" id = "user_name_modal">ユーザー登録</button>
+                    <button type="button" class="btn text-white" style="background-color: #66cc66;WIDTH: 100%" id = "user_name_modal">ユーザー登録</button>
                     <div id='modal'>
                     <div id='content'>
                         <input type='button' value='X' class="close" id='btnClose'/>
-                        <label>名前を入力してください。</label><br/>
-                        <input type='input_user_name' id='input_user_name' required/>
-                        <input type='submit' value='입력' id='btnCheck' onclick="setCookie()"/>
+                        <label>ユーザーネームを入力してください。</label>
+                        <br>
+                        <input class="form-control" type='input_user_name' id='input_user_name' required/>
+                        <br>
+                        <input type='submit' class="btn text-white" style="background-color: #0399e4;" value='登録' id='btnCheck' onclick="setCookie()"/>
                     </div>
                 </div>
                 </div>
             </div>
-    </form><br>
-    <h1 style="text-align: center" id = "user_name"></h1>
+    </form>
+    <h3 style="text-align: center;font-size:16px;font-weight: bold" id = "user_name"></h3>
 </body>
 <script>
 let btnOpen  = document.getElementById('user_name_modal');
@@ -121,17 +134,17 @@ btnClose.onclick = closeRtn;
 		//let cookieArray = cookies.split(";");
 		let cookieArray = cookies.split(/;\s/);
 
-        let request_text = "名前を入力してください。";
-        let request_text_color = request_text.fontcolor("green");
+        // let request_text = "名前を入力してください。";
+        // let request_text_color = request_text.fontcolor("green");
 
 		for (let i = 0; i < cookieArray.length; i++) {
 			let nv = cookieArray[i].split("=");
 			if (nv[0]==cname) { //똑같으면 쿠키 찾아진것
                 //모달창 닫기
                 let UserNameCookie = unescape(nv[1]);
-                let UserNameColor = unescape(nv[1]).fontcolor("blue")
+                let UserNameColor = unescape(nv[1]).fontcolor("#0399e4")
                 $("#user_name_modal").css("visibility", "hidden");
-				$("#user_name").html("歓迎致します " + UserNameColor + "様");
+				$("#user_name").html("こんにちは " + UserNameColor + "様");
                 //unescape: 저장할때 escape로 저장했으니 출력할때 유니코드값을 다시 바꿔주는 것
 				return;
 			}
@@ -141,10 +154,10 @@ btnClose.onclick = closeRtn;
 	}
     function checkCookies(){
         let check_cookies = document.cookie;
-        let request_text = "名前を入力してください。";
+        let request_text = "ユーザー登録をしてください。";
         let request_text_color = request_text.fontcolor("green");
         if(check_cookies === ''){
-            alert("名前を入力してください。");
+            alert("ユーザー登録をしてください。");
             return false;
         }
         else{
