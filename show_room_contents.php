e297466214bb09283832d87a1378abc9c1446bc4
7Yong <95311188+7Yong@users.noreply.github.com>
2022-12-07 13:54:13 +0900

    Update show_room_contents.php

--- show_room_contents.php
+++ show_room_contents.php
@@ -2,7 +2,7 @@
     $room_name = $_GET["room_name"];
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,20 +12,28 @@
     <script src="http://code.jquery.com/jquery-latest.js"></script> 
     <title>ShowRoomContents</title>
     <style>
-    div.fixed {
-        position: fixed;
+    body {
+        font-family: 'Hiragino Kaku Gothic ProN','Hiragino Sans','Meiryo',sans-serif;
     }
+    
     </style>
 </head>
 <body>
-    <h1 style="text-align: center"><img width="388" src="./images/ptitle.png" alt="みんなの症状ことば"></h1><br>
-    <h3 style="text-align: center">頭がズキズキする</h3><br>
-    <h4 style="text-align: center" id = "room_name">
+<div style="line-height:30%">
+<h1 style="text-align: center;font-size:16px">
+    <a href="https://anq.medicalkotoba.com/user_regist.php" height="5" width="10" target="_blank">
+      <img src="./images/newlogo.png" alt="みんなの症状ことば" />
+    <a>
+  </h1><br>
+    <h3 style="text-align: center;font-size:16px;color:#e52a94">頭がズキズキする</h3><br>
+    </div>
+    <h4 style="text-align: center;font-size:16px;color:#0399e4" id = "room_name">
     <?php
     echo "#",$room_name
     ?>
 </h4><br>
-<h4 style="text-align: center">
+
+<h4 style="font-size:14px;color:#333333">
     <?php 
         $host = '13.208.78.196';
         $user = 'yong';
@@ -37,51 +45,55 @@
         $result = $conn->query($sql);
         if(isset($result) && $result->num_rows > 0){
             while($row = $result->fetch_assoc()){
-                echo "@".$row['UserName'];
-                echo "  ".$row['Date']."<br>";
-                echo $row['Content']."<hr>";
+                echo "&nbsp&nbsp&nbsp";
+                echo "@".$row['UserName']."  ".$row['Date']."<br>";
+                //echo "  ".$row['Date']."<br>";
+                echo "&nbsp&nbsp&nbsp";
+                echo $row['Content'];
+                echo "<hr>";
             }
         }
         //style = "float:none;position: fixed;bottom: 100px;"
     ?>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
 </h4>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
+<div style = "background-color:white;" calss="row">
     <form action="room_menu.php" method="get">
         <div calss="row">
-            <div style = "position: fixed;bottom: 100px;" class="col-sm-9">
-            <div style = "position: fixed;bottom: 100px;" class="form-floating col-sm-3">
+            <div style = "position: fixed;bottom: 100px;width: 380px;height: 50px;margin: 0px auto;background-color: white;left: 0;right: 0;text-align: center" class="col-sm-9">
+            <div style = "position: fixed;bottom: 100px;width: 380px;height: 50px;margin: 0px auto;background-color: white;left: 0;right: 0;display:inline-block" class="form-floating col-sm-3">
                 <input type=hidden name=room_name value=<?=$room_name?>>
-                <button type="submit" style = "WIDTH: 150px" class="btn btn-danger" id = "go_menu">他のルームをみる</button>
+                <button type="submit" class="btn text-white" style="background-color: #00bfff;WIDTH: 70%" id = "go_menu">他のルームをみる</button>
             </div>
             </div>
         </div>
     </form>
     <form action="room_write.php" method="get">
         <div calss="row">
-            <div style = "position: fixed;bottom: 50px;" class="col-sm-9">
-            <div style = "position: fixed;bottom: 50px;" class="form-floating col-sm-3">
+            <div style = "position: fixed;bottom: 50px;width: 380px;height: 50px;margin: 0px auto;background-color: white;left: 0;right: 0;text-align: center" class="col-sm-9">
+            <div style = "position: fixed;bottom: 50px;width: 380px;height: 50px;margin: 0px auto;background-color: white;left: 0;right: 0;display:inline-block" class="form-floating col-sm-3">
                 <input type=hidden name=room_name value=<?=$room_name?>>
-                <button type="submit" style = "WIDTH: 150px" class="btn btn-danger" id = "back">ルーム入力</button>
+                <button type="submit"class="btn text-white" style="background-color: #32cd32;WIDTH: 70%" id = "back">新規トークルームの作成</button>
             </div>
             </div>
         </div>
     </form>
     <form action="user_regist.php">
     <div calss="row">
-            <div style = "position: fixed;bottom: 0px;" class="col-sm-9">
-            <div style = "position: fixed;bottom: 0px;" class="form-floating col-sm-3">
-                <button class="btn text-white" style="background-color: purple;WIDTH: 150px;" id = "go_home">閉じる</button>
+            <div style = "position: fixed;bottom: 0px;width: 380px;height: 50px;margin: 0px auto;background-color: white;left: 0;right: 0;text-align: center" class="col-sm-9">
+            <div style = "position: fixed;bottom: 0px;width: 380px;height: 50px;margin: 0px auto;background-color: white;left: 0;right: 0;display:inline-block" class="form-floating col-sm-3">
+                <button class="btn text-white" style="background-color: #c0c0c0;WIDTH: 70%" id = "go_home">閉じる</button>
             </div>
             </div>
     </div>
     </form>
-    
+    </div>
 </body>
 </html>
