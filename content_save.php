70f99fa57950c9e421ab56175ef6305a2f4e96f7
7Yong <95311188+7Yong@users.noreply.github.com>
2022-12-26 15:47:30 +0900

    Update content_save.php

--- content_save.php
+++ content_save.php
@@ -5,14 +5,15 @@
     $dbName = 'ShanriProject';
     $conn = mysqli_connect($host, $user, $pw, $dbName);
 
+    $kw = htmlspecialchars($_REQUEST['kw'], ENT_QUOTES, "UTF-8");
     $room_name = $_GET["room_name"];
-    $user_name = $_COOKIE["user_name"];
+    $user_name = $_GET["user_name"];
     $room_content = $_POST['room_content'];
 
     // $sql = "INSERT INTO created_room (RoomName, UserName, Content) VALUES('$room_name', '$user_name', '$room_content')";
     $check = "SELECT * FROM  ";
-    $stmt = "INSERT INTO created_room (RoomName, UserName, Content) VALUES('$room_name', '$user_name', '$room_content')";
+    $stmt = "INSERT INTO new_created_room (kw, RoomName, UserName, Content) VALUES('$kw','$room_name', '$user_name', '$room_content')";
     mysqli_query($conn, $stmt);
 
-    echo "<script>location.replace('show_room_contents.php?room_name=$room_name')</script>";
+    echo "<script>location.replace('show_room_contents.php?kw=$kw&room_name=$room_name&user_name=$user_name')</script>";
 ?>
